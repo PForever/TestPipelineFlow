ae540eda9348eb2e1ab2bb97dc34bc88473b83cf
Igor Ganov <igor_ganov@live.ru>
2022-02-16 16:44:45 +0300

    draft

--- TestPipelineFlow/Program.cs
+++ TestPipelineFlow/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-#region Domain
+#region DomainModels
 
 class RecoLine
 {
@@ -60,8 +60,17 @@ public class DomainLogic
     {
         _now = DateTime.UtcNow;
 
+        ResultState state = ResultState.AlreadyAlerted | ResultState.DbSaveFailed;
+
+        state.CompareTo(ResultState.AlreadyAlerted)
+
+
+        Item.With(ResultState.Unknown).Handle<RecoLine>(MarkAlreadyAlerted1).Out(ResultState.AlreadyAlerted)
+
+
         _ = await _repository.GetNotSyncedLines()
         .HandleEach(ResultState.Unknown, MarkAlreadyAlerted)
+        .HandleEach(Item.With(ResultState.Unknown).Handle(MarkAlreadyAlerted).Out(ResultState.AlreadyAlerted))
         .HandleEachNot(ResultState.AlreadyAlerted, MarkReadyToSync)
         .HandleEach(ResultState.ReadyToSync, TryToSync)
         .HandleEach(ResultState.SyncSuccess, SaveSyncSuccedState)
@@ -72,6 +81,12 @@ public class DomainLogic
     }
 
     #region Handlers
+    Result<RecoLine, ResultState> MarkAlreadyAlerted1(Result<RecoLine, ResultState> recoLine)
+        => recoLine.Value.SyncState.AlertedOn is not null ? recoLine
+        with { State = ResultState.AlreadyAlerted } :
+        recoLine;
+
+
     ResultState MarkAlreadyAlerted(RecoLine recoLine) 
         => recoLine.SyncState.AlertedOn is not null ? ResultState.AlreadyAlerted : ResultState.Unknown;
 
@@ -126,7 +141,7 @@ public class DomainLogic
 
 #region Infrastructure
 
-readonly record struct Result<T>(T Value, ResultState State, Exception? Exception);
+readonly record struct Result<T, TState>(T Value, TState State, Exception? Exception);
 [Flags]
 enum ResultState
 {
@@ -138,27 +153,74 @@ enum ResultState
     DbSaveFailed = 16,
 }
 
+struct HandleNode<TInputState, TValue>
+{
+    public HandleNode(TInputState inputState, Func<Result<TValue, TInputState>, Result<TValue, TInputState>> handler, TInputState outputState) : this()
+    {
+        Input = inputState;
+        Output = outputState;
+        Handler = handler;
+    }
+
+    public TInputState Input { get; }
+    public TInputState Output { get; }
+    public Func<Result<TValue, TInputState>, Result<TValue, TInputState>> Handler  { get; }
+}
+
+//Item.With(ResultState.Unknown).Handle(MarkAlreadyAlerted).Out(ResultState.AlreadyAlerted)
+static class Item
+{
+    public static Input<TInputState> With<TInputState, TValue>(this IEnumerable<TValue> _, TInputState inputState) => new(inputState);
+}
+class Input<TInputState>
+{
+    private readonly TInputState _inputState;
+
+    public Input(TInputState inputState)
+    {
+        _inputState = inputState;
+    }
+    public Handler<TInputState, TValue> Handle<TValue>(Func<Result<TValue, TInputState>, Result<TValue, TInputState>> handler) => new(_inputState, handler);
+}
+
+internal class Handler<TInputState, TValue>
+{
+    private readonly TInputState _inputState;
+    private readonly Func<Result<TValue, TInputState>, Result<TValue, TInputState>> _handler;
+
+    public Handler(TInputState inputState, Func<Result<TValue, TInputState>, Result<TValue, TInputState>> handler)
+    {
+        _inputState = inputState;
+        _handler = handler;
+    }
+    public HandleNode<TInputState, TValue> Out(TInputState outputState) => new(_inputState, _handler, outputState);
+}
+
+
+
 static class Extantions
 {
-    private static Func<Result<T>, Result<T>> ToHandlerResult<T>(Func<T, ResultState> handler) => r => handler(r.Value) is var newState && newState != ResultState.Unknown ? new Result<T>(r.Value, newState, null) : r;
-    private static Func<Result<T>, ValueTask<Result<T>>> ToHandlerResult<T>(Func<T, ValueTask<ResultState>> handler) => async r => await handler(r.Value) is var newState && newState != ResultState.Unknown ? new Result<T>(r.Value, newState, null) : r;
 
+    private static Func<Result<T, TState>, Result<T, TState>> ToHandlerResult<T, TState>(Func<T, ResultState> handler) => r => handler(r.Value) is var newState && newState != ResultState.Unknown ? new Result<T, TState>(r.Value, newState, null) : r;
+    private static Func<Result<T, TState>, ValueTask<Result<T, TState>>> ToHandlerResult<T, TState>(Func<T, ValueTask<ResultState>> handler) => async r => await handler(r.Value) is var newState && newState != ResultState.Unknown ? new Result<T, TState>(r.Value, newState, null) : r;
 
-    public static IEnumerable<Result<T>> HandleEach<T>(this IEnumerable<Result<T>> src, ResultState handleState, Func<Result<T>, Result<T>> handler)
+    public static IEnumerable<Result<T, TState>> HandleEach<T, TState>(this IEnumerable<Result<T, TState>> src, ResultState handleState, Func<Result<T, TState>, Result<T, TState>> handler)
+    where TState : 
     {
         foreach (var item in src.Where(i => (i.State & handleState) == i.State))
         {
             yield return handler(item);
         }
     }
-    public static IEnumerable<Result<T>> HandleEachNot<T>(this IEnumerable<Result<T>> src, ResultState handleState, Func<Result<T>, Result<T>> handler)
+    public static IEnumerable<Result<T, TState>> HandleEachNot<T, TState>(this IEnumerable<Result<T, TState>> src, ResultState handleState, Func<Result<T, TState>, Result<T, TState>> handler)
+    where TState 
     {
         foreach (var item in src.Where(i => (i.State & handleState) != i.State))
         {
             yield return handler(item);
         }
     }
-    public static IReadOnlyCollection<T> HandleAll<T>(this IEnumerable<Result<T>> src, ResultState handleState, Action<IEnumerable<T>> action)
+    public static IReadOnlyCollection<T> HandleAll<T, TState>(this IEnumerable<Result<T, TState>> src, ResultState handleState, Action<IEnumerable<T>> action)
     {
         var list = src.Where(i => (i.State & handleState) == i.State).Select(i => i.Value).ToList();
         action(list);
@@ -168,8 +230,8 @@ static class Extantions
 
     
 
-    public static IAsyncEnumerable<Result<T>> HandleEach<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<T, ResultState> handler) => HandleEach(src, handleState, ToHandlerResult(handler));
-    public static async IAsyncEnumerable<Result<T>> HandleEach<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<Result<T>, Result<T>> handler)
+    public static IAsyncEnumerable<Result<T, TState>> HandleEach<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<T, ResultState> handler) => HandleEach(src, handleState, ToHandlerResult(handler));
+    public static async IAsyncEnumerable<Result<T, TState>> HandleEach<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<Result<T, TState>, Result<T, TState>> handler)
     {
         await foreach (var item in src)
         {
@@ -178,8 +240,8 @@ static class Extantions
         }
     }
 
-    public static IAsyncEnumerable<Result<T>> HandleEach<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<T, ValueTask<ResultState>> handler) => HandleEach(src, handleState, ToHandlerResult(handler));
-    public static async IAsyncEnumerable<Result<T>> HandleEach<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<Result<T>, ValueTask<Result<T>>> handler)
+    public static IAsyncEnumerable<Result<T, TState>> HandleEach<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<T, ValueTask<ResultState>> handler) => HandleEach(src, handleState, ToHandlerResult(handler));
+    public static async IAsyncEnumerable<Result<T, TState>> HandleEach<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<Result<T, TState>, ValueTask<Result<T, TState>>> handler)
     {
         await foreach (var item in src)
         {
@@ -191,8 +253,8 @@ static class Extantions
 
 
 
-    public static IAsyncEnumerable<Result<T>> HandleEachNot<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<T, ResultState> handler) => HandleEachNot(src, handleState, ToHandlerResult(handler));
-    public static async IAsyncEnumerable<Result<T>> HandleEachNot<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<Result<T>, Result<T>> handler)
+    public static IAsyncEnumerable<Result<T, TState>> HandleEachNot<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<T, ResultState> handler) => HandleEachNot(src, handleState, ToHandlerResult(handler));
+    public static async IAsyncEnumerable<Result<T, TState>> HandleEachNot<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<Result<T, TState>, Result<T, TState>> handler)
     {
         await foreach (var item in src)
         {
@@ -201,7 +263,7 @@ static class Extantions
         }
     }
 
-    public static async Task<IReadOnlyCollection<T>> HandleAll<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Action<IReadOnlyCollection<T>> action)
+    public static async Task<IReadOnlyCollection<T>> HandleAll<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Action<IReadOnlyCollection<T>> action)
     {
         var list = new List<T>();
         await foreach (var item in src)
@@ -212,7 +274,7 @@ static class Extantions
         return list;
     }
 
-    public static async Task<IReadOnlyCollection<T>> HandleAll<T>(this IAsyncEnumerable<Result<T>> src, ResultState handleState, Func<IReadOnlyCollection<T>, Task> action)
+    public static async Task<IReadOnlyCollection<T>> HandleAll<T>(this IAsyncEnumerable<Result<T, TState>> src, ResultState handleState, Func<IReadOnlyCollection<T>, Task> action)
     {
         var list = new List<T>();
         await foreach (var item in src)
@@ -222,8 +284,6 @@ static class Extantions
         await action(list);
         return list;
     }
-
-
 
     #endregion
 }
